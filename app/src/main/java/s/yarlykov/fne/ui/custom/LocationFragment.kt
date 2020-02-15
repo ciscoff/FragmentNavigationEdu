b01ebc723cd2938661d03498047e4fb01aa3dad2
ciscoff <ciscoff@ya.ru>
2020-02-15 09:42:15 +0300

    initial commit

---
+++ app/src/main/java/s/yarlykov/fne/ui/custom/LocationFragment.kt
@@ -0,0 +1,19 @@
+package s.yarlykov.fne.ui.custom
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import s.yarlykov.fne.R
+
+class LocationFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_location, container, false)
+    }
+}
---
+++ app/src/main/java/s/yarlykov/fne/ui/custom/PriceFragment.kt
@@ -0,0 +1,18 @@
+package s.yarlykov.fne.ui.custom
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import s.yarlykov.fne.R
+
+class PriceFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_price, container, false)
+    }
+}
