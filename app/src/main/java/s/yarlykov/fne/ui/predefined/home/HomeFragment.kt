b456a8dc903b248287117d08b86a7874cff35c26
ciscoff <ciscoff@ya.ru>
2020-02-16 19:27:44 +0300

    Подсветка кнопок ОК

--- app/src/main/java/s/yarlykov/fne/ui/predefined/home/HomeFragment.kt
+++ app/src/main/java/s/yarlykov/fne/ui/predefined/home/HomeFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -17,7 +18,7 @@ class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
     private lateinit var navController: NavController
-    private lateinit var buttons : Map<Int, Button>
+    private lateinit var buttons: Map<Int, Button>
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -43,6 +44,7 @@ class HomeFragment : Fragment() {
         }
 
         /**
+         * Ищем NavHostFragment (и его NavController) внутри фрагмента Home
          * https://developer.android.com/reference/androidx/navigation/fragment/NavHostFragment
          */
         val navHostFragment = childFragmentManager.findFragmentById(R.id.nav_ticket_controller)
@@ -58,26 +60,31 @@ class HomeFragment : Fragment() {
         /**
          * Кликнутая кнопка становится disabled
          */
-        fun disableClicked(id: Int) {
-            buttons.entries.forEach {
-                it.value.isEnabled = id != it.key
-                it.value.setBackgroundResource(
-                    if(it.value.isEnabled)
-                        R.drawable.background_button_inactive
-                    else
-                        R.drawable.background_button_active
-                )
+        fun highlightClicked(clickedButtonId: Int) {
+            buttons.entries.forEach { mapEntry ->
+
+                with(mapEntry.value) {
+
+                    if (clickedButtonId == mapEntry.key) {
+                        setBackgroundResource(R.drawable.background_button_active)
+                        setTextColor(ContextCompat.getColor(context, R.color.buttonActive))
+
+                    } else {
+                        setBackgroundResource(R.drawable.background_button_inactive)
+                        setTextColor(ContextCompat.getColor(context, R.color.buttonInactive))
+                    }
+                }
             }
         }
 
         /**
          * При клике на кнопку переключаем фрагмент и дизейблим кнопку
          */
-        fun setButtonClickListener(buttonId : Int) {
-            with(buttonId){
+        fun setButtonClickListener(buttonId: Int) {
+            with(buttonId) {
                 buttons[this]?.setOnClickListener {
                     navController.navigate(this)
-                    disableClicked(this)
+                    highlightClicked(this)
                 }
             }
         }
