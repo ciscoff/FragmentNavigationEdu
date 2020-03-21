0a9bd53dda8e7c63541d4289050cb7c3532e8d6d
ciscoff <ciscoff@ya.ru>
2020-03-21 20:48:19 +0300

    add timeactivity

---
+++ app/src/main/java/s/yarlykov/fne/ui/custom/timeline/TimeActivity.kt
@@ -0,0 +1,13 @@
+package s.yarlykov.fne.ui.custom.timeline
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import s.yarlykov.fne.R
+
+class TimeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_time)
+    }
+}
