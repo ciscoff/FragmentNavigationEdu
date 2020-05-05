40ae4e854be759852b583c5f9abf12db7e828ca6
ciscoff <ciscoff@ya.ru>
2020-05-05 22:19:48 +0300

    parallax

--- app/src/main/java/s/yarlykov/fne/ui/telegram/ChatActivity.kt
+++ app/src/main/java/s/yarlykov/fne/ui/telegram/ChatActivity.kt
@@ -2,25 +2,42 @@ package s.yarlykov.fne.ui.telegram
 
 import android.os.Bundle
 import android.view.View
-import android.view.WindowManager
-import com.google.android.material.snackbar.Snackbar
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.google.android.material.appbar.AppBarLayout
+import com.google.android.material.appbar.CollapsingToolbarLayout
 import s.yarlykov.fne.R
-
-import kotlinx.android.synthetic.main.activity_chat.*
 import s.yarlykov.fne.ui.telegram.data.ClientMessage
 import s.yarlykov.fne.ui.telegram.data.DoerMessage
 
 class ChatActivity : AppCompatActivity() {
 
     private val model = listOf(
+        ClientMessage("Добрый день !", "24.04, 18:17"),
+        DoerMessage("Здравствуйте !", "24.04, 18:18"),
+        ClientMessage("Добрый день !", "24.04, 18:19"),
+        DoerMessage("Здравствуйте !", "24.04, 18:20"),
+        ClientMessage("Добрый день !", "24.04, 18:21"),
+        DoerMessage("Здравствуйте !", "24.04, 18:22"),
+        ClientMessage("Добрый день !", "24.04, 18:23"),
+        DoerMessage("Здравствуйте !", "24.04, 18:25"),
+        ClientMessage("Добрый день !", "24.04, 18:27"),
+        DoerMessage("Здравствуйте !", "24.04, 18:30"),
+        ClientMessage("Добрый день !", "24.04, 18:32"),
+        DoerMessage("Здравствуйте !", "24.04, 18:33"),
+        ClientMessage("Добрый день !", "24.04, 18:34"),
+        DoerMessage("Здравствуйте !", "24.04, 18:35"),
         ClientMessage("Добрый день !", "24.04, 18:37"),
         DoerMessage("Здравствуйте !", "24.04, 18:40")
     )
+
+    private var isExpanded = true
 
     companion object {
         const val KEY_CHAT_ID = "KEY_CHAT_ID"
@@ -30,21 +47,45 @@ class ChatActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_client_chat)
 
+        val appBarLayout = findViewById<AppBarLayout>(R.id.chat_appbar)
+
         val toolbar: Toolbar = findViewById<View>(R.id.toolbar_shared) as Toolbar
         setSupportActionBar(toolbar)
 
-        // Скрываем Status Bar (полноэкранный режим)
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        supportActionBar?.title = "Чат с клиентом"
-        toolbar.setNavigationIcon(R.drawable.ic_apps)
+        val collapsingToolbar = findViewById<CollapsingToolbarLayout>(R.id.collapsing_toolbar)
+        collapsingToolbar.title = getString(R.string.action_settings)
+
+
+        toolbar.setOnClickListener {
+            isExpanded = !isExpanded
+            appBarLayout.setExpanded(isExpanded, true)
+        }
+
+        initRecyclerView()
+
+        loadBackdrop()
+
+        // Скрываем Status Bar (полноэкранный режим)
+//        window.setFlags(
+//            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+//            WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
+//        supportActionBar?.title = "Чат с клиентом"
+//        toolbar.setNavigationIcon(R.drawable.ic_apps)
+
+
 
         getExtras()
 
 
-        initRecyclerView()
+    }
+
+    private fun loadBackdrop() {
+        val imageView = findViewById<ImageView>(R.id.backdrop)
+        Glide.with(this).load(R.drawable.bkg_01_jan)
+            .apply(RequestOptions.centerCropTransform()).into(imageView)
     }
 
     // Получить chatId
