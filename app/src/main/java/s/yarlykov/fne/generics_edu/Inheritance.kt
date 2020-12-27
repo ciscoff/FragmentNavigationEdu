b738721112c8cadb449d188122ad7c4ea51bb28f
ciscoff <ciscoff@ya.ru>
2020-12-27 14:18:25 +0300

    ok

--- app/src/main/java/s/yarlykov/fne/generics_edu/Inheritance.kt
+++ app/src/main/java/s/yarlykov/fne/generics_edu/Inheritance.kt
@@ -1,4 +1,30 @@
 package s.yarlykov.fne.generics_edu
 
-class Inheritance {
+
+open class Item<T : Any> {
+
+    private lateinit var d : T
+
+    fun put(data : T) {
+
+    }
+    fun fetch() : T {
+        return d
+    }
 }
+
+class ItemChild<T : Any> : Item<T>() {
+
+}
+
+fun useItem(item : Item<*>) {
+    println("${item::class.java.simpleName}")
+}
+
+fun main(args : Array<String>) {
+
+    useItem(ItemChild<String>())
+
+}
+
+
